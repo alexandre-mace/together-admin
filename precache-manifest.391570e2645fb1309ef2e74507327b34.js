ed604a5805ab20570e154e726934d8d2ab4243f0
alexandre-mace <contact.alexandremace@gmail.com>
2020-05-22 10:22:35 +0200

    Updates

---
+++ precache-manifest.391570e2645fb1309ef2e74507327b34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b120d46c4c0ba9f377b48e69b5ef7ce",
+    "url": "/together-admin/index.html"
+  },
+  {
+    "revision": "4a1fbfa2d62b1f569557",
+    "url": "/together-admin/static/js/2.29039737.chunk.js"
+  },
+  {
+    "revision": "700c9a55bfe350adac2e777cd13ef7b0",
+    "url": "/together-admin/static/js/2.29039737.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b41feb6407a567833367",
+    "url": "/together-admin/static/js/main.fd26e895.chunk.js"
+  },
+  {
+    "revision": "26061f75b139499bfb7e",
+    "url": "/together-admin/static/js/runtime-main.e02520ea.js"
+  }
+]);
